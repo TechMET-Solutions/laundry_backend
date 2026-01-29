ed1151f0df75b91461b39373ccf6bacee7dbc7f5
Sakshi Peharkar <sakshipeharkar@gmail.com>
2026-01-29 16:46:02 +0530

    apis of expense category

--- src/controllers/expenses.controller.js
+++ src/controllers/expenses.controller.js
@@ -148,3 +148,115 @@ exports.deleteExpenses = async (req, res) => {
     }
 };
 
+//EXPENSE CATEGORY CONTROLLERS
+
+//✅ CREATE EXPENSE CATEGORY
+exports.createExpenseCategory=async(req,res)=>{
+    
+    try{
+        const{category_name,description}=req.body;
+        const createTableSQL=`
+        create table if not exists expense_categories(
+            id int auto_increment primary key,
+            category_name varchar(100) not null,
+            description varchar(255),
+            createdAt timestamp default current_timestamp
+        )
+        `;
+        await db.query(createTableSQL);
+        const insertSQL=`
+        insert into expense_categories
+        (
+            category_name,
+            description
+        )
+        values(?,?)
+        `;
+        const [result]=await db.query(insertSQL,[category_name,description]);
+        res.status(201).json({success:true,message:"Expense category created successfully",id:result.insertId});    
+    }catch(err){
+        res.status(500).json({success:false,error:err.message});
+    }
+};
+
+//✅ GET ALL EXPENSE CATEGORIES
+exports.getAllExpenseCategories=async(req,res)=>{
+    try{
+        const [rows]=await db.query(`SELECT * FROM expense_categories ORDER BY id DESC`);
+        res.json({success:true,data:rows});
+    }
+    catch(err){
+        res.status(500).json({succes:false,error:err.message});
+    }
+}
+
+
+
+//✅ GET EXPENSE CATEGORY BY ID
+exports.getExpenseCategoryById=async(req,res)=>{
+    try{
+        const{id}=req.params;
+        //const[rows]=await db.query(`select * from expense_catogories where id=? ${id}`)
+         const [rows] = await db.query(`SELECT * FROM expense_categories WHERE id =?`, [id]);
+
+        if(!rows.length){
+            return res.status(404).json({success:false,message:"Expense category not found"});
+        }
+        res.status(400).json({succes:true,data:rows[0]});
+    }
+    catch(err){
+        res.status(500).json({success:false,error:err.message});
+    }
+}
+
+// ✅ DELETE Expences
+exports.deleteExpenseCategory = async (req, res) => {
+    try {
+        const { id } = req.params;  
+        const deleteSQL = `DELETE FROM expense_categories WHERE id = ?`;
+        const [result] = await db.query(deleteSQL, [id]);   
+        if (result.affectedRows === 0) {
+            return res
+                .status(404)
+                .json({ success: false, message: "Expences not found" });
+        }
+        res.json({ success: true, message: "Expences deleted successfully" });
+    } catch (err) {
+        res.status(500).json({ success: false, error: err.message });
+    }
+};
+
+
+// ✅ UPDATE Expences
+exports.updateExpenseCategory = async (req, res) => {
+    try {
+        const { id } = req.params;  
+        const {
+            category_name,
+            description
+        } = req.body;  
+        const updateSQL=`
+        update expense_categories set
+        category_name=?,
+        description=?
+        where id=?
+        `;
+
+        const [result] = await db.query(updateSQL,[
+            category_name,
+            description,
+            id
+        ]); 
+
+         if (result.affectedRows === 0) {
+            return res
+                .status(404)
+                .json({ success: false, message: "Expense category not found" });
+        }
+        res.json({ success: true, message: "Expenses category updated successfully" });
+    } catch (err) {
+        res.status(500).json({ success: false, error: err.message });
+    }
+};
+
+
--- src/routes/expenses.routes.js
+++ src/routes/expenses.routes.js
@@ -23,4 +23,27 @@ router.put("/update/:id", updateExpenses);
 // DELETE
 router.delete("/delete/:id", deleteExpenses);
 
+//API of Expense category
+const{
+    createExpenseCategory,
+    getAllExpenseCategories,
+    getExpenseCategoryById,
+    updateExpenseCategory,
+    deleteExpenseCategory,
+}=require("../controllers/expenses.controller");
+//create category
+router.post("/category/create",createExpenseCategory);  
+
+//get all category
+router.get("/category/list",getAllExpenseCategories);
+
+//get category by id
+router.get("/category/list/:id",getExpenseCategoryById);
+
+//update category
+router.put("/category/update/:id",updateExpenseCategory);
+
+//delete category
+router.delete("/category/delete/:id",deleteExpenseCategory);
+
 module.exports = router;
